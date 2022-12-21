87295b89f14320d4ff96ef25caa71f65375a3b22
Eliška Pokorná <pokornaeliska25@gmail.com>
2022-12-21 19:20:07 +0100

    Projekt Lhota

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function css() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+}
 
 exports.css = css;
